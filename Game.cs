f37b73f4e634d25d2545218ca2ad36185bd33b3f
Dai LiRong <xhyzdai@qq.com>
2012-04-18 13:38:36 +0800

    first commit

---
+++ Game.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameTest
+{
+	class Game
+	{
+		private int[] rolls = new int[21];
+		private int currentRoll = 0;
+		internal void roll(int pins)
+		{
+			rolls[currentRoll++] = pins;
+		}
+
+		internal int score()
+		{
+			int scores = 0;
+			int frameIndex = 0;
+			for (int frame = 0; frame < 10; ++frame)
+			{
+				if (isStrike(frameIndex))
+				{
+					scores += 10 + strikeBonus(frameIndex);
+					frameIndex++;
+				}
+				else if (isSpare(frameIndex))
+				{
+					scores += 10 + spareBonus(frameIndex);
+					frameIndex += 2;
+				}
+				else
+				{
+					scores += sumOfBallsInFrame(frameIndex);
+					frameIndex += 2;
+				}
+			}
+			return scores;
+		}
+
+		private int sumOfBallsInFrame(int frameIndex)
+		{
+			return rolls[frameIndex] + rolls[frameIndex + 1];
+		}
+
+		private int strikeBonus(int frameIndex)
+		{
+			return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+		}
+
+		private int spareBonus(int frameIndex)
+		{
+			return rolls[frameIndex + 2];
+		}
+
+		private bool isSpare(int frameIndex)
+		{
+			return 10 == rolls[frameIndex] + rolls[frameIndex + 1];
+		}
+
+		private bool isStrike(int frameIndex)
+		{
+			return rolls[frameIndex] == 10;
+		}
+	}
+}
---
+++ GameTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameTest
+{
+	[TestClass]
+	public class GameTest
+	{
+		private void rollMany(int n, int pins)
+		{
+			for (int i = 0; i < n; i++)
+				game.roll(pins);
+		}
+		Game game = new Game();
+
+		[TestMethod]
+		public void TestOnlyOne()
+		{
+			game.roll(5);
+			Assert.AreEqual(game.score(), 5);
+		}
+
+		[TestMethod]
+		public void TestAllZeros()
+		{
+			rollMany(20, 0);
+			Assert.AreEqual(0, game.score());
+		}
+
+		[TestMethod]
+		public void TestAllOnes()
+		{
+			rollMany(20, 1);
+			Assert.AreEqual(game.score(), 20);
+		}
+
+		[TestMethod]
+		public void TestOneSpare()
+		{
+			game.roll(5);
+			game.roll(5); // spare
+			game.roll(3);
+			rollMany(17,0);
+			Assert.AreEqual(16,game.score());
+		}
+
+		[TestMethod]
+		public void TestOneStrike()
+		{
+			game.roll(10); // strike
+			game.roll(3);
+			game.roll(4);
+			rollMany(16, 0);
+			Assert.AreEqual(24, game.score());
+		}
+
+		[TestMethod]
+		public void TestPerfectGame()
+		{
+			rollMany(12, 10);
+			Assert.AreEqual(300, game.score());
+		}
+
+		[TestMethod]
+		public void TestTypicalGame()
+		{
+			game.roll(1);
+			game.roll(4);
+			game.roll(4);
+			game.roll(5);
+			game.roll(6);
+			game.roll(4);
+			game.roll(5);
+			game.roll(5);
+			game.roll(10);
+			game.roll(0);
+			game.roll(1);
+			game.roll(7);
+			game.roll(3);
+			game.roll(6);
+			game.roll(4);
+			game.roll(10);
+			game.roll(2);
+			game.roll(8);
+			game.roll(6);
+			Assert.AreEqual(133, game.score());
+		}
+	}
+}
